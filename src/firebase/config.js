18e1f82e8cb850f2280ea6ee20365f3feff491c2
YuhaoXie <yuhao14@vt.edu>
2021-04-22 18:59:38 -0400

    changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,13 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+    apiKey: "AIzaSyBbsmPAEWQb8DxR9qWxNc81_HoNVwtoOew",
+    authDomain: "mobile-app-9c89b.firebaseapp.com",
+    projectId: "mobile-app-9c89b",
+    storageBucket: "mobile-app-9c89b.appspot.com",
+    messagingSenderId: "57837637908",
+    appId: "1:57837637908:web:3feea6e346d65e4c4bf201",
+    measurementId: "G-MPJ7NDBLDB"
 };
 
 if (!firebase.apps.length) {
